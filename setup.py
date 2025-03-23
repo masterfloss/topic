d2e371ec938850314cad310c1c914409e1e73aeb
masterfloss <carlos.costa@acm.org>
2025-03-23 03:01:01 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='topic',  # Replace with your module's name
+    version='1.0.0',
+    py_modules=['playscript'],  # The name of your Python file (without .py)
+    description='Module examplifying',
+    author='Carlos J. Costa',
+    author_email='carlos.costa@ieee.org',
+    url='https://github.com/masterfloss/topic',
+)
