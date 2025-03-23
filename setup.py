75804864742cbceefdd5fa71ed6ffc5421b355c4
masterfloss <carlos.costa@acm.org>
2025-03-23 03:32:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='generatetopic',  # Replace with your module's name
     version='1.0.0',
-    py_modules=['playscript'],  # The name of your Python file (without .py)
+    py_modules=['generatetopic'],  # The name of your Python file (without .py)
     description='Module examplifying',
     author='Carlos J. Costa',
     author_email='carlos.costa@ieee.org',
